555388c2bf813d0c46fc83a899c2f05b39416036
sunshine2329 <seniordeveloper00001@gmail.com>
2018-06-17 22:10:08 -0400

    Nesting Components

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>{{ title }} </h1>
+    <ninjas></ninjas>
+  </div>
+</template>
+
+<script>
+
+import Ninjas from './Ninjas'
+
+export default {
+  components: {
+    'ninjas': Ninjas
+  },
+  data () {
+    return {
+      title: 'Ninja App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
