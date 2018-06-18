4c1af40012c13e0c83490261bc15e55ac7b7cdc2
sunshine2329 <seniordeveloper00001@gmail.com>
2018-06-17 23:36:36 -0400

    The Event Bus

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+import { bus } from '../main';
+
 export default {
   props: {
     title: {
@@ -15,6 +17,11 @@ export default {
     return {
       copyright: 'Copyright 2018'
     }
+  },
+  created() {
+    bus.$on('titleChanged', (data) => {
+      this.title = data;
+    });
   }
 }
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+import { bus } from '../main';
+
 export default {
     props: {
         title: {
@@ -18,7 +20,9 @@ export default {
   },
   methods: {
     changeTitle: function() {
-      this.$emit('changeTitle', 'Vue Wizards');
+      // this.$emit('changeTitle', 'Vue Wizards');
+      this.title = 'Vue Wizards';
+      bus.$emit('titleChanged', 'Vue Wizards');
     }
   }
 }
