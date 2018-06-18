baf1185f26eab36b74ded89760b5b78e79193908
sunshine2329 <seniordeveloper00001@gmail.com>
2018-06-18 16:49:26 -0400

    HTTP Request

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="add-blog">
       <h2>Add a New Blog post</h2>
-      <form>
+      <form v-if="!submitted">
           <label>Blog Title:</label>
           <input type="text" v-model.lazy="blog.title" required />
           <label>Blog Content:</label>
@@ -20,7 +20,11 @@
           <select v-model="blog.author">
               <option v-for="(author, index) in authors" v-bind:key="`blog-author-${index}`">{{ author }}</option>
           </select>
+          <button v-on:click.prevent="post">Add Blog</button>
       </form>
+      <div v-if="submitted">
+          <h3>Thanks for adding your post</h3>
+      </div>
       <div id="preview">
           <h3>Preview Blog</h3>
           <p>Blog title: {{ blog.title }}</p>
@@ -46,10 +50,21 @@ export default {
             categories: [],
             author: ""
         },
-        authors: ['The Net Ninja', 'The Angular Avenger', 'The Vue Vindicator']
+        authors: ['The Net Ninja', 'The Angular Avenger', 'The Vue Vindicator'],
+        submitted: false
     }
   },
   methods: {
+      post: function() {
+          this.$http.post('http://jsonplaceholder.typicode.com/posts', {
+              title: this.blog.title,
+              body: this.blog.content,
+              userId: 1
+          }).then(function(data) {
+              console.log(data);
+              this.submitted = true;
+          });
+      }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource'
 
-export const bus = new Vue();
+Vue.use(VueResource);
 
 new Vue({
     el: '#app',
