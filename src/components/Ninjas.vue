02297cb9c0714210c2cb9db05f1aa9e4d84a79c9
sunshine2329 <seniordeveloper00001@gmail.com>
2018-06-17 23:45:37 -0400

    Lifecycle Hooks

--- src/components/Ninjas.vue
+++ src/components/Ninjas.vue
@@ -27,6 +27,25 @@ export default {
     deleteNinja: function() {
       this.ninjas.pop();
     }
+  },
+  // lifecycle hooks
+  beforeCreate() {
+    alert('beforeCreate');
+  },
+  created() {
+    alert('created');
+  },
+  beforeMount() {
+    alert('beforeMount');
+  },
+  mounted() {
+    alert('mounted');
+  },
+  beforeUpdate() {
+    alert('beforeUpdate');
+  },
+  updated() {
+    alert('updated');
   }
 }
 </script>
