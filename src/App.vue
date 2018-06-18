c69f51c9ba2ddaf3074f70e391875e9f0f0d37ee
sunshine2329 <seniordeveloper00001@gmail.com>
2018-06-18 16:19:58 -0400

    Input binding(Creating a blog)

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
   <div>
-    <keep-alive>
-      <component v-bind:is="component"></component>
-    </keep-alive>
-    <button v-on:click="component = 'form-one'">Show form one</button>
-    <button v-on:click="component = 'form-two'">Show form two</button>
+    <add-blog></add-blog>
   </div>
 </template>
 
 <script>
-
-import formOne from './components/formOne';
-import formTwo from './components/formTwo';
+import addBlog from './components/addBlog';
 
 export default {
   components: {
-    'form-one': formOne,
-    'form-two': formTwo
+    'add-blog': addBlog
   },
   data () {
     return {
-      component: 'form-one'
     }
   },
   methods: {
@@ -28,5 +20,9 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
+body {
+  margin: 0;
+  font-family: "Nunito SemiBold";
+}
 </style>
---
+++ src/components/addBlog.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="add-blog">
+      <h2>Add a New Blog post</h2>
+      <form>
+          <label>Blog Title:</label>
+          <input type="text" v-model.lazy="blog.title" required />
+          <label>Blog Content:</label>
+          <textarea v-model.lazy="blog.content"></textarea>
+      </form>
+      <div id="preview">
+          <h3>Preview Blog</h3>
+          <p>Blog title: {{ blog.title }}</p>
+          <p>Blog content:</p>
+          <p>{{ blog.content }}</p>
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+        blog: {
+            title: "",
+            content: ""
+        }
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+#add-blog * {
+  box-sizing: border-box;
+}
+#add-blog {
+  margin: 20px auto;
+  max-width: 500px;
+}
+label {
+  display: block;
+  margin: 20px 0 10px;
+}
+input[type="text"],
+textarea {
+  display: block;
+  width: 100%;
+  padding: 8px;
+}
+#preview {
+  padding: 10px 20px;
+  border: 1px dotted #ccc;
+  margin: 30px 0;
+}
+h3 {
+  margin-top: 10px;
+}
+</style>
