87aea3c373bb209ea375e93e33eaf583ea67ea2c
sunshine2329 <seniordeveloper00001@gmail.com>
2018-06-17 23:07:26 -0400

    Props

--- src/components/Ninjas.vue
+++ src/components/Ninjas.vue
@@ -11,16 +11,15 @@
 
 <script>
 export default {
+  props: {
+    ninjas: {
+      type: Array,
+      required: true
+    }
+  },
   data () {
     return {
-      ninjas: [
-        {name: 'Ryu', speciality: 'Vue Components', show: false},
-        {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
-        {name: 'Hitoshi', speciality: 'Click Events', show: false},
-        {name: 'Tango', speciality: 'Conditionals', show: false},
-        {name: 'Kami', speciality: 'Webpack', show: false},
-        {name: 'Yoshi', speciality: 'Data Diggin', show: false}
-      ]
+      
     }
   }
 }
