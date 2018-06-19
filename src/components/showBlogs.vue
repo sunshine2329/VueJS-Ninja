493f216a231ffc2b0346d8130c5ab1b1a18da8e6
sunshine2329 <seniordeveloper00001@gmail.com>
2018-06-18 22:59:43 -0400

    Filter

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -1,9 +1,9 @@
 <template>
-  <div v-theme:column="'narrow'" id="show-blogs">
+  <div v-theme:column="'wide'" id="show-blogs">
       <h1>All Blog Articles</h1>
       <div v-for="(blog, index) in blogs" class="single-blog" v-bind:key="`blog-${index}`">
-          <h2 v-rainbow>{{ blog.title }}</h2>
-          <article>{{ blog.body }}</article>
+          <h2 v-rainbow>{{ blog.title | to-uppercase }}</h2>
+          <article>{{ blog.body | snippet }}</article>
       </div>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -26,6 +26,15 @@ Vue.directive('theme', {
     }
 });
 
+// Filters
+Vue.filter('to-uppercase', function(value) {
+    return value.toUpperCase();
+});
+
+Vue.filter('snippet', function(value) {
+    return value.slice(0, 100) + "...";
+});
+
 new Vue({
     el: '#app',
     render: h => h(App)
