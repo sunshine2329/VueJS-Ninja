53155f6ecd17bc9aa1cccb8e258ed99d612ff3dc
sunshine2329 <seniordeveloper00001@gmail.com>
2018-06-19 01:55:59 -0400

    Retrieving Posts from Firebase

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -4,7 +4,7 @@
       <input tyoe="text" v-model="search" placeholder="search blogs" />
       <div v-for="(blog, index) in filteredBlogs" class="single-blog" v-bind:key="`blog-${index}`">
           <router-link v-bind:to="'/blog/' + blog.id"><h2 v-rainbow>{{ blog.title | to-uppercase }}</h2></router-link>
-          <article>{{ blog.body | snippet }}</article>
+          <article>{{ blog.content | snippet }}</article>
       </div>
   </div>
 </template>
@@ -22,9 +22,15 @@ export default {
   methods: {
   },
   created() {
-      this.$http.get('http://jsonplaceholder.typicode.com/posts').then(function(data){
-          console.log(data);
-          this.blogs = data.body.slice(0, 10);
+      this.$http.get('https://vuejs-ninja.firebaseio.com/post.json').then(function(data){
+          return data.json();
+      }).then(function(data){
+          var blogsArray = [];
+          for (let key in data) {
+              data[key].id = key;
+              blogsArray.push(data[key]);
+          }
+          this.blogs = blogsArray;
       });
   },
   computed: {
@@ -55,5 +61,8 @@ export default {
   margin: 20px 0;
   box-sizing: border-box;
   background: #eee;
+}
+a {
+  text-decoration: none;
 }
 </style>
--- src/components/singleBlog.vue
+++ src/components/singleBlog.vue
@@ -1,7 +1,11 @@
 <template>
     <div id="single-blog">
         <h1>{{ blog.title }}</h1>
-        <article>{{ blog.body }}</article>
+        <article>{{ blog.content }}</article>
+        <p>Author: {{ blog.author }}</p>
+        <ul>
+            <li v-for="(category, index) in blog.categories" v-bind:key="`single-blog-${index}`">{{ category }}</li>
+        </ul>
     </div>
 </template>
 
@@ -15,9 +19,10 @@ export default {
     },
     created() {
         console.log("Hello");
-        this.$http.get('http://jsonplaceholder.typicode.com/posts/' + this.id).then(function(data) {
-            console.log(data);
-            this.blog = data.body;
+        this.$http.get('https://vuejs-ninja.firebaseio.com/post/' + this.id + ".json").then(function(data) {
+            return data.json();
+        }).then(function(data){
+            this.blog = data;
         });
     }
 }
