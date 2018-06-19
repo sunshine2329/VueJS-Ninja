e3375456a1c9716d06545f43fbd722b5f7c1c278
sunshine2329 <seniordeveloper00001@gmail.com>
2018-06-18 23:24:53 -0400

    Mixins

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <show-blogs></show-blogs>
+    <list-blogs></list-blogs>
   </div>
 </template>
 
 <script>
 import addBlog from './components/addBlog';
 import showBlogs from './components/showBlogs';
+import listBlogs from './components/listBlogs';
 
 export default {
   components: {
     'add-blog': addBlog,
-    'show-blogs': showBlogs
+    'show-blogs': showBlogs,
+    'list-blogs': listBlogs
   },
   data () {
     return {
--- src/components/listBlogs.vue
+++ src/components/listBlogs.vue
@@ -1,10 +1,9 @@
 <template>
   <div v-theme:column="'wide'" id="show-blogs">
-      <h1>All Blog Articles</h1>
+      <h1>List Blog Titles</h1>
       <input tyoe="text" v-model="search" placeholder="search blogs" />
       <div v-for="(blog, index) in filteredBlogs" class="single-blog" v-bind:key="`blog-${index}`">
           <h2 v-rainbow>{{ blog.title | to-uppercase }}</h2>
-          <article>{{ blog.body | snippet }}</article>
       </div>
   </div>
 </template>
